6a9e4746a01fc535dcd9292c53c8671102336eac
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-18 14:51:29 -0800

    default data now reflects on input fields

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Education(props) {
   const [institution, setInstitution] = useState("");
@@ -6,6 +6,17 @@ function Education(props) {
   const [location, setLocation] = useState("");
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+
+  useEffect(() => {
+    if (!(props.education.institution === "")) {
+      setInstitution(props.education.institution);
+      setCertification(props.education.certification);
+      setLocation(props.education.location);
+      setFrom(props.education.from);
+      setTo(props.education.to);
+    }
+  }, [props.education]);
+
   return (
     <div className="Education">
       <form className="educational-form">
@@ -13,30 +24,35 @@ function Education(props) {
           type="text"
           id="education-university"
           placeholder="Educational Institution Attended"
+          value={institution}
           onChange={(e) => setInstitution(e.target.value)}
         />
         <input
           type="text"
           id="education-degree"
           placeholder="Certification or Degree Earned"
+          value={certification}
           onChange={(e) => setCertification(e.target.value)}
         />
         <input
           type="text"
           id="education-location"
           placeholder="State or City Of Institution"
+          value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
         <input
           type="text"
           id="experience-from"
           placeholder="Start Date"
+          value={from}
           onChange={(e) => setFrom(e.target.value)}
         />
         <input
           type="text"
           id="education-to"
           placeholder="End Date"
+          value={to}
           onChange={(e) => setTo(e.target.value)}
         />
       </form>
--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PersonalInformation(props) {
   const [fullName, setFullName] = useState("");
@@ -7,6 +7,18 @@ function PersonalInformation(props) {
   const [personalPhone, setPersonalPhone] = useState("");
   const [personalEmail, setPersonalEmail] = useState("");
   const [personalDescription, setPersonalDescription] = useState("");
+
+  useEffect(() => {
+    if (!(props.personalInformation.fullName === "")) {
+      setFullName(props.personalInformation.fullName);
+      setPersonalTitle(props.personalInformation.personalTitle);
+      setPersonalAddress(props.personalInformation.personalAddress);
+      setPersonalPhone(props.personalInformation.personalPhone);
+      setPersonalEmail(props.personalInformation.personalEmail);
+      setPersonalDescription(props.personalInformation.personalDescription);
+    }
+  }, [props.personalInformation]);
+
   return (
     <div className="PersonalInformation">
       <form className="personalInformation-form">
@@ -15,36 +27,42 @@ function PersonalInformation(props) {
           type="text"
           id="full-name"
           placeholder="Full Name"
+          value={fullName}
           onChange={(e) => setFullName(e.target.value)}
         />
         <input
           type="text"
           id="personal-title"
           placeholder="Current Title"
+          value={personalTitle}
           onChange={(e) => setPersonalTitle(e.target.value)}
         />
         <input
           type="text"
           id="personal-address"
           placeholder="Current Address"
+          value={personalAddress}
           onChange={(e) => setPersonalAddress(e.target.value)}
         />
         <input
           type="text"
           id="personal-phone"
           placeholder="Phone Number"
+          value={personalPhone}
           onChange={(e) => setPersonalPhone(e.target.value)}
         />
         <input
           type="text"
           id="personal-email"
           placeholder="Current Email Address"
+          value={personalEmail}
           onChange={(e) => setPersonalEmail(e.target.value)}
         />
         <textarea
           type="text"
           id="personal-description"
           placeholder="Description Of Yourself"
+          value={personalDescription}
           onChange={(e) => setPersonalDescription(e.target.value)}
         />
       </form>
